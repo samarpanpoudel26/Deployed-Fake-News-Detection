c9c685e7606b9719b586ff7991cc77f0fc6ffaa8
samarpanpoudel26 <168333729+samarpanpoudel26@users.noreply.github.com>
2025-04-16 20:33:17 -0400

    Update model1.py

--- model1.py
+++ model1.py
@@ -1,62 +1,95 @@
+# %%
 import pandas as pd
 import nltk
-import os
-import re
-import string
-import streamlit as st
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import re
+import string
 from sklearn.feature_extraction.text import CountVectorizer 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 
-# ======== Download NLTK resources safely & persistently ========
-nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-nltk.data.path.append(nltk_data_path)
 
-for resource in ['punkt', 'stopwords', 'wordnet']:
-    try:
-        if resource == 'punkt':
-            nltk.data.find(f'tokenizers/{resource}')
-        else:
-            nltk.data.find(f'corpora/{resource}')
-    except LookupError:
-        nltk.download(resource, download_dir=nltk_data_path)
+# %%
+dataset=pd.read_csv("data.xls")
 
-# ======== Preprocessing Function ========
+# %%
+dataset.head(3)
+
+# %%
+dataset.isnull().sum()
+
+# %%
+dataset.drop(columns="URLs",axis=1,inplace=True)
+dataset.drop(columns="Headline",axis=1,inplace=True)
+
+# %%
+dataset.dropna(inplace=True)
+
+# %%
+dataset.isnull().sum()
+
+# %%
+dataset=dataset.sample(frac=1)
+
+# %%
 def preprocessing(text):
+    """
+    Function that does all the steps of Natural Language Processing.
+    
+    """
+    
     text = text.lower()
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = re.sub(r'\d+', '', text)
     tokens = word_tokenize(text)
     stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words and len(word) > 2]
+    tokens = [word for word in tokens if word not in stop_words]
     cleaned_text = " ".join(tokens)
+   
     return cleaned_text
 
-# ======== Load + Train Model (without caching for now) ========
-def load_model():
-    dataset = pd.read_csv("data.xls")
-    dataset.drop(columns=["URLs", "Headline"], axis=1, inplace=True)
-    dataset.dropna(inplace=True)
-    dataset = dataset.sample(frac=1)
-    dataset["Body"] = dataset["Body"].apply(preprocessing)
+# %%
+dataset["Body"]=dataset["Body"].apply(preprocessing)
 
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(dataset["Body"])
-    y = dataset["Label"]
+# %%
+vectorizer=CountVectorizer()
 
-    x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
-    model = MultinomialNB()
-    model.fit(x_train, y_train)
+# %%
+X=vectorizer.fit_transform(dataset["Body"])
 
-    return model, vectorizer
+# %%
+Y=dataset[["Label"]]
 
-# ======== Predict Function Used by app.py ========
-def user_input(text):
-    model, vectorizer = load_model()
-    cleaned = preprocessing(text)
-    transformed = vectorizer.transform([cleaned])
-    prediction = model.predict(transformed)
-    return int(prediction[0])
+# %%
+x_train,x_test,y_train,y_test=train_test_split(X,Y,random_state=42,test_size=0.2)
+
+# %%
+mnb=MultinomialNB()
+mnb.fit(x_train,y_train)
+
+# %%
+mnb.score(x_test,y_test)*100,mnb.score(x_train,y_train)*100
+
+# %%
+def user_input(user_text):
+    """
+    Function to Predict User Input
+    """
+      
+    cleaned_text = preprocessing(user_text)  # Preprocess it
+    transformed_text = vectorizer.transform([cleaned_text])  # Convert to numerical format
+    prediction = mnb.predict(transformed_text)# Predict
+    
+    if prediction==1:
+        print("Not a fake News")
+    else:
+        print("Fake news")
+    return prediction
+
+
+
